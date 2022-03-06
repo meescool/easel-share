fd2d662fea9cddf7b88fb343e1697a8b4dc7a0e9
maestr01 <maestr01@wsc.edu>
2022-03-06 06:37:22 -0600

    Finish basic canvas functionality
    The easel page is functional. There are still issues when drawing, but I
    have the settings ready so that the user is able to change the color,
    widths, erase and are able to clear the canvas to start over.
    
    Issues to be closed:
    - add stroke color, closes #11
    - add stroke sizes, closes # 12
    - add ability to draw, closes #15

---
+++ public/js/canvas.js
@@ -0,0 +1,84 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let x = 0;
+let y = 0;
+
+let x2 = 0;
+let y2 = 0;
+
+let drawing = false;
+
+let color = '000';
+let size = 1;
+
+erase = false;
+
+bounds = canvas.getBoundingClientRect();
+
+function changeColor(newColor){
+    color = newColor;
+}
+
+function changeSize(newSize){
+    size = newSize;
+}
+
+function eraseAll(e){
+    if(e =1){
+        erase = true;
+
+    }
+
+    if (erase == true){
+        ctx.clearRect(0,0,canvas.width, canvas.height);
+        erase = false;
+    }
+}
+
+const eraseBtn = document.getElementById('eraseAll');
+
+eraseBtn.addEventListener('click', function(e){
+    console.log('clear')
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+});
+
+canvas.addEventListener('mousedown', function(e){
+     x2 = e.clientX - bounds.left;
+     y2 = e.clientY - bounds.top;
+     drawing = true;
+     render();
+
+});
+
+canvas.addEventListener(('mousemove'), function(e){
+    
+    if(drawing == true){
+    x = e.clientX - bounds.left;
+    y = e.clientY - bounds.top;
+    render();
+    }
+});
+
+canvas.addEventListener(('mouseup'), function(e){
+    x2 = e.clientX - bounds.left;
+    y2 = e.clientY - bounds.top;
+    drawing = false;
+    
+});
+
+
+
+
+
+function render(){
+
+    ctx.lineWidth= size;
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+
+    
+}
