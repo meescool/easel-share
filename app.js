357e71e1c9322cb643c9c4ee4f6a65c3696908ba
maestr01 <maestr01@wsc.edu>
2022-03-06 01:50:55 -0600

    Start ejs templates and styling
    Started working on adding the necessary templates for the first
    prototype of the website. All the links in the menu are succesfully
    linked, and have placeholders for the logo and the profile picture.

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const lodash = require("lodash");
 
 const app = express();
 
+const pages=[['learning', 'Learning'],['art-shows','Art Shows'],['user-page','User Page'], ['easel','My Easel']];
+
 const data = {
     titleName:'Home',
-    theme: 'night',
     url: '/'
 }
 
@@ -22,11 +23,30 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
     log('this is home');
     data.titleName = 'Home';
-    data.url='/';
-    res.render('index',data);
+    data.url='index';
+    res.render(data.url,data);
+})
+
+app.get('/:pagename', function(req, res){
+    let tempName = lodash.toLower(req.params.pagename);
+    // set to 404 in case page does not exist
+    data.titleName = '404';
+    data.url='404';
+    // loop through each of the pages available to set the next page
+    pages.forEach(function(page){
+        if(page[0] == tempName){
+            console.log(page[0]);
+            data.titleName = page[1];
+            data.url = page[0];
+        }
+    });    
+    
+    res.render(data.url,data);
 })
 
 app.listen(3000, function(){
